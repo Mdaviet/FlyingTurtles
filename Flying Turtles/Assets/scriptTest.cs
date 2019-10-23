5148fcd5847b089d0cb66f4ccdee48ce352017de
Garrett Ness <garrettness97@gmail.com>
2019-10-23 16:54:22 -0600

    Forgot to add new files, oops

---
+++ Flying Turtles/Assets/scriptTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptTest : MonoBehaviour
+{
+	
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hewwo!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(Input.GetMouseButtonDown(0)){
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+			
+			RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+			
+			if(hit.collider != null){
+				Debug.Log("Clicked " + hit.collider.gameObject.name);
+				if(hit.collider.gameObject.GetComponent<Renderer>().material.color != Color.red)
+					hit.collider.gameObject.GetComponent<Renderer>().material.color = Color.red;
+				else
+					hit.collider.gameObject.GetComponent<Renderer>().material.color = Color.white;
+			}
+			
+        }
+    }
+	
+
+	
+	
+}
